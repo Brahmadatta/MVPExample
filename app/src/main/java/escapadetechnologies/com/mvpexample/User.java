70e5a4ed80e77b5f62891731242714685651420a
user <g.brahmadatta@gmail.com>
2019-04-15 17:19:46 +0530

    first commit'

---
+++ app/src/main/java/escapadetechnologies/com/mvpexample/User.java
@@ -0,0 +1,37 @@
+package escapadetechnologies.com.mvpexample;
+
+public class User {
+
+    private String fullName = "";
+
+    private String email = "";
+
+    public User(String fullName, String email) {
+        this.fullName = fullName;
+        this.email = email;
+    }
+
+    public User() {
+    }
+
+    public String getFullName() {
+        return fullName;
+    }
+
+    public void setFullName(String fullName) {
+        this.fullName = fullName;
+    }
+
+    public String getEmail() {
+        return email;
+    }
+
+    public void setEmail(String email) {
+        this.email = email;
+    }
+
+    @Override
+    public String toString() {
+        return "Email: " + email + "\nFullName: " +fullName;
+    }
+}
